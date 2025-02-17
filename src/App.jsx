2a510063fafc3e8532e80772d5be9dfb44312ec4
gacay <victoriahuynh2011@gmail.com>
2025-02-17 16:27:32 -0500

    Footer updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import './navBar.css';
 import WhatIsWitcon from './sections/WhatIsWitCon';
 import FAQs from './sections/FAQs';
 import Hero from './sections/Hero.jsx';
+import Instagram from './assets/Instagram.png';
+import LinkedIn from './assets/Linkedin.png';
+import Twitter from './assets/Twitter.png';
+import Facebook from './assets/Facebook.png';
 
 function App() {
   return (
@@ -33,9 +37,20 @@ function App() {
         <FAQs></FAQs>
       </div>
 
-      
         <Homepage></Homepage>
       </Layout>
+
+      <div className='footer-container text-base text-white font-[Pixelify_Sans]'>
+        <div className="footer-content">
+        <a href="" className="footer-link">Stay Up To Date with our News and Notifications</a>
+        <div className="social-icons">
+        <a href="https://www.instagram.com/wicsfiu/?hl=en"><img className='icon-img' src={Instagram} alt="Instagram" /></a>
+        <a href="https://www.linkedin.com/company/wicsatfiu/"><img className='icon-img' src={LinkedIn} alt="LinkedIn" /></a>
+        <a href="https://x.com/WICSFIU"><img className='icon-img' src={Twitter} alt="Twitter" /></a>
+        <a href="https://www.facebook.com/wicysfiu/"><img className='icon-img' src={Facebook} alt="Facebook" /></a>
+          </div>
+        </div>
+      </div>
 
     </>
   )
