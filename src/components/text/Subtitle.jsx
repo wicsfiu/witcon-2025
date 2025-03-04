0690f6b947f4e29b33d079f5660a81c4554ac561
Fer Pacheco <fernipm@gmail.com>
2025-03-04 00:51:35 -0500

    fix: resizing and background fix for sponsor logos

--- src/components/text/Subtitle.jsx
+++ src/components/text/Subtitle.jsx
@@ -1,3 +1,3 @@
 export default function Subtitle({ children }) {
-    return <h2 className="text-2xl font-semibold text-white font-[Pixelify_Sans]">{children}</h2>;
+    return <h2 className="text-3xl font-semibold text-white font-[Pixelify_Sans]">{children}</h2>;
   }
--- src/sections/Sponsors.jsx
+++ src/sections/Sponsors.jsx
@@ -1,35 +1,31 @@
 import React from 'react';
 import Subtitle from '../components/text/Subtitle.jsx';
-import MegaSponsor from '../assets/Company logos/MEGABYTE.png';
-import MegaSponsor2 from '../assets/Company logos/MEGABYTE (2).png';
-import MegaSponsor3 from '../assets/Company logos/MEGABYTE (3).png';
-import Sponsor4 from '../assets/Company logos/4.png';
-import Sponsor5 from '../assets/Company logos/5.png';
-import Sponsor6 from '../assets/Company logos/6.png';
-import Sponsor7 from '../assets/Company logos/7.png';
-import Sponsor8 from '../assets/Company logos/8.png';
-import communityPartner from '../assets/Company logos/community partner.png';
-import communityPartner2 from '../assets/Company logos/community partner (2).png';
-import communityPartner3 from '../assets/Company logos/community partner (3).png';
+import Akamai from '../assets/Company logos/Akamai.png';
+import CapitalOne from '../assets/Company logos/CapitalOne.png';
+import BreakThroughTech from '../assets/Company logos/BreakThroughTech.png';
+import Cisco from '../assets/Company logos/Cisco.png';
+import Google from '../assets/Company logos/Google.png';
+import UKG from '../assets/Company logos/UKG.png';
+import Microsoft from '../assets/Company logos/Microsoft.png';
+import CodePath from '../assets/Company logos/CodePath.png';
+import FIUCEC from '../assets/Company logos/FIUCEC.png';
+import RewriteTheCode from '../assets/Company logos/RewriteTheCode.png';
 
-const megaSponsors = [
-    { name: "Chevron", logo: MegaSponsor },
-    { name: "Akamai", logo: MegaSponsor2 },
-    { name: "Capital One", logo: MegaSponsor3 }
-]
 
 const sponsors = [
-    { name: "Google", logo: Sponsor4 },
-    { name: "Microsoft", logo: Sponsor5 },
-    { name: "Cisco", logo: Sponsor6 },
-    { name: "UKG", logo: Sponsor7 },
-    { name: "Break Through Tech", logo: Sponsor8 }
+    { name: "Akamai", logo: Akamai },
+    { name: "Capital One", logo: CapitalOne },
+    { name: "Google", logo: Google },
+    { name: "Microsoft", logo: Microsoft },
+    { name: "Cisco", logo: Cisco },
+    { name: "UKG", logo: UKG },
 ];
 
 const communitySponsors = [
-    { name: "Code Path Org", logo: communityPartner },
-    { name: "Rewriting the Code", logo: communityPartner2 },
-    { name: "FIU KFSCIS", logo: communityPartner3 }
+    { name: "Break Through Tech", logo: BreakThroughTech },
+    { name: "Code Path Org", logo: CodePath },
+    { name: "Rewriting the Code", logo: RewriteTheCode },
+    { name: "FIU College of Engineering and Computing", logo: FIUCEC }
 ]
 
 export default function SponsorsSection() {
@@ -42,19 +38,20 @@ export default function SponsorsSection() {
             </div>
 
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 gap-10 items-center justify-center">
-                {megaSponsors.map((sponsor, index) => (
-                    <div key={index} className="flex justify-center">
-                        <img src={sponsor.logo} alt={sponsor.name} className="h-92 w-auto object-contain"/>
-                    </div>
-                ))}
-
                 {sponsors.map((sponsor, index) => (
                     <div key={index} className="flex justify-center">
                         <img src={sponsor.logo} alt={sponsor.name} className="h-52 w-auto object-contain"/>
                     </div>
                 ))}
+            </div>
 
-                {communitySponsors.map((sponsor, index) => (
+            <div>
+                <Subtitle className="flex space-x-3 py-10 place-content-center">
+                    Our Community Partners
+                </Subtitle>
+            </div>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 gap-10 items-center justify-center">
+            {communitySponsors.map((sponsor, index) => (
                     <div key={index} className="flex justify-center">
                         <img src={sponsor.logo} alt={sponsor.name} className="h-42 w-auto object-contain"/>
                     </div>
